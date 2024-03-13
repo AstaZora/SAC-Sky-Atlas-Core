e460514ede99ce52696f358ab8925ca1eb97b23b
Asta Zora <Tas_King@outlook.com>
2024-03-12 20:04:07 -0400

    Update for kits, and Discord Webhook

--- prototypes/item/item.lua
+++ prototypes/item/item.lua
@@ -8,5 +8,103 @@ data:extend({
         order = "b[personal-transport]-c[spidertron]-a[spider]",
         place_result = "mcv",
         stack_size = 1
-    }
+    },
+    {
+        type = "item",
+        name = "arboretum-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
+    {
+        type = "item",
+        name = "aquaculture-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+    },
+    {
+        type = "item",
+        name = "hydroponics-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
+    {
+        type = "item",
+        name = "greenhouse-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
+    {
+        type = "item",
+        name = "farm-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
+    {
+        type = "item",
+        name = "orchard-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
+    {
+        type = "item",
+        name = "plantation-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
+    {
+        type = "item",
+        name = "surveyor-base-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
+    {
+        type = "item",
+        name = "drill-site-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
+    {
+        type = "item",
+        name = "resource-outpost-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
+    {
+        type = "item",
+        name = "expeditionary-mining-rig-kit",
+        icon = "__base__/graphics/icons/satellite.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "transport",
+        order = "b[personal-transport]-c[spidertron]-a[spider]",
+        stack_size = 1
+    },
 })
